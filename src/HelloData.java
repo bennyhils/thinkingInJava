0fec5a1bb1c55dbaccf892621d89c130f6f064ff
bennyhils <bennyhils@ya.ru>
2018-11-27 18:37:29 +0700

    first commit

---
+++ src/HelloData.java
@@ -0,0 +1,10 @@
+public class HelloData {
+    public static void main(String[] args) {
+        Character iOb  = 'v';
+        method(iOb);
+    }
+
+    public static void method(char i) {
+        System.out.println(i);
+    }
+}
