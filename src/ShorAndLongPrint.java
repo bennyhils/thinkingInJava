f5e7b021ea0456f74fa5ea235d5c12a33ae7b0e3
bennyhils <bennyhils@ya.ru>
2018-11-27 19:09:14 +0700

    + done

---
+++ src/ShorAndLongPrint.java
@@ -0,0 +1,8 @@
+//: operators/ShorAndLongPrint.java
+
+public class ShorAndLongPrint {
+    public static void main(String[] args) {
+        System.out.println("Длинная печать");
+        Print.print("Короткая печать");
+    }
+}
